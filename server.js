b728263d621a9cf6592840ffe1f6acc89e77cc44
resghst <hank181017@gmail.com>
2018-05-21 22:14:51 +0800

    use socket io to maintain playlist finish real time playing video..

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ var http = require("http");
 var url = require('url');
 var fs = require('fs');
 var io = require('socket.io'); // 加入 Socket.IO
+let filepath  = './playlist/playlist.json'
 
 var server = http.createServer(function(request, response) {
   console.log('Connection');
+  let initplaylist ={
+      'data': [],
+      'currentposit': 0,
+      'novideo': true,
+      'videotime': new Date(),
+      'push': (id)=> playlist.data.push(id),
+      'deletevideo': (id)=>	playlist.data.push(id)
+  }
+  fs.readFile( filepath , 'utf8', function (err, buffer) { 
+    buffer = JSON.parse(buffer)
+    if(Object.keys(buffer).length === 0) fs.writeFile( filepath , JSON.stringify(initplaylist), 'utf8')
+  })
   var path = url.parse(request.url).pathname;
-
+ 
   switch (path) {
     case '/':
       response.writeHead(200, {'Content-Type': 'text/html'});
@@ -34,11 +47,15 @@ var server = http.createServer(function(request, response) {
 });
 
 var serv_io = io.listen(server);
-serv_io.set('log level', 1); // 關閉 debug 訊息
+// serv_io.set('log level', 1); // 關閉 debug 訊息
 
 serv_io.sockets.on('connection', function(socket) {
+  let initplaylist ={ }
   setInterval(function() {
-    socket.emit('date', {'date': new Date()});
+    fs.readFile( filepath , 'utf8', function(err, buffer){  initplaylist = JSON.parse(buffer) })
+    // var dateObj = {'reqdate': new Date()}
+    // dateObj.resdate =  new Date()
+    socket.emit('date', initplaylist);
   }, 1000);
 
   // 接收來自於瀏覽器的資料
@@ -48,3 +65,18 @@ serv_io.sockets.on('connection', function(socket) {
 });
 
 server.listen(8001);
+
+
+
+
+
+
+// var sleep = (milliseconds)=>{
+//   var start = new Date().getTime();
+//   for (var i = 0; i < 1e7; i++)
+//     if ((new Date().getTime() - start) > milliseconds){
+//       console.log("waiting")
+//       break 
+//       }
+//   return new Date()
+// }
